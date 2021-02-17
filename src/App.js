273df00b9051d2e73d1d62fda46a28c0cfe5d5fd
samuelmarina <samuellmarina@gmail.com>
2021-02-17 02:02:40 -0400

    Just do everything

--- src/App.js
+++ src/App.js
@@ -1,22 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from "@material-ui/core"
+import { useState } from 'react';
+import rickRoll from "./rick-roll.jpg"
 
 function App() {
+  const [b1, setb1] = useState(true);
+  const [b2, setb2] = useState(false);
+  const [b3, setb3] = useState(false);
+  const [b4, setb4] = useState(false);
+  const [rick, setRick] = useState(false);
+
+
+  const changeVisibility = (number) => {
+    switch (number) {
+      case 1:
+        setb1(false);
+        setb2(true)
+        break;
+      case 2:
+        setb2(false);
+        setb3(true);
+        break;
+      case 3:
+        setb3(false);
+        setb4(true);
+        break;
+      default:
+        setb4(false);
+        setRick(true);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {b1 ? (
+          <Button variant="contained" color="primary" onClick={() => changeVisibility(1)}>
+            Click to get the code
+          </Button>
+       ) : null}
+       {b2 ? (
+         <Button variant="contained" color="primary" onClick={() => changeVisibility(2)}>
+            One more time
+          </Button>
+       ) : null}
+       {b3 ? (
+         <Button variant="contained" color="primary" onClick={() => changeVisibility(3)}>
+            Lmao just one more time
+          </Button>
+       ) : null}
+       {b4 ? (
+         <Button variant="contained" color="primary" onClick={() => changeVisibility(4)}>
+            I promise just one more time
+          </Button>
+       ) : null}
+       {rick ? (
+         <img src={rickRoll} alt="Rick" />
+       ) : null}
       </header>
     </div>
   );
